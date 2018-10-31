9b31732115d726e24d17a6f27526ab054879eb1c
jueyaye <dan_2212@hotmail.com>
2018-11-01 02:16:31 +1000

    update

--- src/problem/Main.java
+++ src/problem/Main.java
@@ -56,7 +56,7 @@ public class Main {
         Level level = ps.getLevel();
 
         for(ActionType actionType : level.getAvailableActions()){
-            double res = 0;
+            double res;
 
             switch(actionType) {
                 case MOVE:
@@ -64,13 +64,13 @@ public class Main {
 
                     double sum = 0.0;
                     rSet = allSet.get(currentState).getScore();
+                    int reqFuel = Util.getFuelConsumption(currentState, ps);
 
-                    for (int i = 0; i < moveProbs.length; i++) {
+                    if(Util.checkMoveCondition(ps, currentState)) {
 
-                        if(Util.checkMoveCondition(ps, currentState)) {
+                        for (int i = 0; i < moveProbs.length; i++) {
+
                             if(i < 10){
-
-                                int reqFuel = Util.getFuelConsumption(currentState, ps);
 
                                 projectedState = currentState.changePosition((i - 4), ps.getN());
                                 projectedState = projectedState.consumeFuel(reqFuel);
@@ -88,20 +88,19 @@ public class Main {
                                             (double)(ps.getSlipRecoveryTime() - 1));
                                 }else{
                                     sum += moveProbs[i] * vNext * Math.pow(ps.getDiscountFactor(),
-                                            (double)(ps.getSlipRecoveryTime() - 1));
+                                            (double)(ps.getRepairTime() - 1));
                                 }
                             }
                         }
+
+                        res = rSet + ps.getDiscountFactor() * sum;
+
+                        if(max <= res){
+                            max = res;
+                            act = new Action(ActionType.MOVE);
+                            ret = new ScoredAction(act, res);
+                        }
                     }
-
-                    act = new Action(ActionType.MOVE);
-                    res = rSet + ps.getDiscountFactor() * sum;
-
-                    if(max <= res){
-                        max = res;
-                        ret = new ScoredAction(act, res);
-                    }
-
                     break;
 
                 case CHANGE_CAR:
@@ -113,15 +112,14 @@ public class Main {
                             rSet = allSet.get(currentState).getScore();
 
                             res = rSet + ps.getDiscountFactor()*vNext;
-                            act = new Action(ActionType.MOVE);
 
                             if(max <= res){
                                 max = res;
+                                act = new Action(ActionType.CHANGE_CAR, car);
                                 ret = new ScoredAction(act, res);
                             }
                         }
                     }
-
                     break;
 
                 case CHANGE_DRIVER:
@@ -133,18 +131,17 @@ public class Main {
                             rSet = allSet.get(currentState).getScore();
 
                             res = rSet + ps.getDiscountFactor()*vNext;
-                            act = new Action(ActionType.MOVE);
 
                             if(max <= res){
                                 max = res;
+                                act = new Action(ActionType.CHANGE_DRIVER, driver);
                                 ret = new ScoredAction(act, res);
                             }
                         }
                     }
-
                     break;
-                case CHANGE_TIRES:
 
+                case CHANGE_TIRES:
                     for(Tire tires : ps.getTireOrder()){
                         if(!tires.equals(currentState.getTireModel())){
                             projectedState = currentState.changeTires(tires);
@@ -153,34 +150,33 @@ public class Main {
                             rSet = allSet.get(currentState).getScore();
 
                             res = rSet + ps.getDiscountFactor()*vNext;
-                            act = new Action(ActionType.MOVE);
 
                             if(max <= res){
                                 max = res;
+                                act = new Action(ActionType.CHANGE_TIRES, tires);
                                 ret = new ScoredAction(act, res);
                             }
                         }
                     }
-
                     break;
-                case ADD_FUEL:
 
-                    projectedState = currentState.addFuel(10); // add fuel where cost is 1 time unit
+                case ADD_FUEL:
+                    int fuel = Math.min(10, 50 - currentState.getFuel());
+                    projectedState = currentState.addFuel(fuel); // add fuel where cost is 1 time unit
 
                     vNext = currentSet.get(projectedState).getScore();
                     rSet = allSet.get(currentState).getScore();
 
                     res = rSet + ps.getDiscountFactor()*vNext;
-                    act = new Action(ActionType.MOVE);
 
                     if(max <= res){
                         max = res;
+                        act = new Action(ActionType.ADD_FUEL, fuel);
                         ret = new ScoredAction(act, res);
                     }
-
                     break;
-                case CHANGE_PRESSURE:
 
+                case CHANGE_PRESSURE:
                     for(TirePressure tp : TirePressure.values()){
                         if(!tp.equals(currentState.getTirePressure())){
                             projectedState = currentState.changeTirePressure(tp);
@@ -189,48 +185,49 @@ public class Main {
                             rSet = allSet.get(currentState).getScore();
 
                             res = rSet + ps.getDiscountFactor()*vNext;
-                            act = new Action(ActionType.MOVE);
 
                             if(max <= res){
                                 max = res;
+                                act = new Action(ActionType.CHANGE_PRESSURE, tp);
                                 ret = new ScoredAction(act, res);
                             }
                         }
                     }
-
                     break;
+
                 case CHANGE_CAR_AND_DRIVER:
-
                     for(String car : ps.getCarOrder()){
-                        for(String driver : ps.getDriverOrder()){
-                            if(!car.equals(currentState.getCarType()) &&
-                                !driver.equals(currentState.getDriver())){
-                                projectedState = currentState.changeCarAndDriver(car, driver);
+                        if (!car.equals(currentState.getCarType())) {
+                            for (String driver : ps.getDriverOrder()) {
+                                if (!driver.equals(currentState.getDriver())) {
+                                    projectedState = currentState.changeCarAndDriver(car, driver);
 
-                                vNext = currentSet.get(projectedState).getScore();
-                                rSet = allSet.get(currentState).getScore();
+                                    vNext = currentSet.get(projectedState).getScore();
+                                    rSet = allSet.get(currentState).getScore();
 
-                                res = rSet + ps.getDiscountFactor()*vNext;
-                                act = new Action(ActionType.MOVE);
+                                    res = rSet + ps.getDiscountFactor() * vNext;
 
-                                if(max <= res){
-                                    max = res;
-                                    ret = new ScoredAction(act, res);
+                                    if (max <= res) {
+                                        max = res;
+                                        act = new Action(ActionType.CHANGE_CAR_AND_DRIVER, car, driver);
+                                        ret = new ScoredAction(act, res);
+                                    }
                                 }
                             }
                         }
                     }
-
                     break;
+
                 default:
-
-                    System.out.println("you wrong");
+                    System.out.println("Cannot Run Level 5");
                     System.exit(9);
-
                     break;
             }
         }
-
+        if(ret == null){
+            System.out.println("you bad");
+            System.exit(69);
+        }
         return ret;
     }
 
@@ -241,7 +238,7 @@ public class Main {
         HashMap<State, ScoredAction> allStates = genStates(ps);   // that R(s) good shit
 
         HashMap<State, ScoredAction> current = (HashMap<State, ScoredAction>) allStates.clone(); // v(s)
-        HashMap<State, ScoredAction> next = new HashMap();
+        HashMap<State, ScoredAction> next = new HashMap(); // v[t+1](s)
 
         boolean hasConverged = false;
 
@@ -257,15 +254,18 @@ public class Main {
                 double vdash = res.getScore();
 
                 next.put(state, res);
-
-                if(hasConverged && (Math.abs(v - vdash) > 0.00001)){
+                if(hasConverged && (Math.abs(v - vdash) > 0.0001)) {
                     System.out.println(v + " + " + vdash);
                     hasConverged = false;
+                }
+                if ((System.nanoTime() - startTime)/1000000 > 119000) {
+                    System.out.println("Time's up!");
+                    hasConverged = true;
+                    break;
                 }
             }
 
             current = (HashMap<State, ScoredAction>) next.clone();
-            next.clear();
         }
 
         long endTime = System.nanoTime();
@@ -274,19 +274,92 @@ public class Main {
         State start = sim.reset();  // initial state
         System.out.println(current.get(start));
 
-        while(!sim.isGoalState(start)){
-
+        while(!sim.isGoalState(start) && start != null){
             ScoredAction step = current.get(start);
             start = sim.step(step.getAction());
         }
     }
 
+    public static ActionType MCTS(ProblemSpec ps, State currentState, int currentStep){
+        long startTime = System.nanoTime();
+        long endTime = System.nanoTime();
+        ActionType choice = null;
+        while((endTime - startTime) / 1000000 < 14000){
+            Level level = ps.getLevel();
+
+
+
+            endTime = System.nanoTime();
+        }
+        return choice;
+    }
+
+    public static boolean simulate(ProblemSpec ps, State simState, int currentStep){
+
+        int req;
+        int move;
+
+        while(currentStep <= ps.getMaxT()){
+            req = Util.getFuelConsumption(simState, ps);
+            if(simState.getFuel() < req){
+                simState = simState.addFuel(10);
+                currentStep++;
+            }
+            else{
+                move = sampleMoveDistance(ps, simState);
+                if(move == 6){
+                    currentStep += ps.getSlipRecoveryTime();
+                }
+                else if(move == 7){
+                    currentStep += ps.getRepairTime();
+                }
+                else{
+                    simState = simState.changePosition(move, ps.getN());
+                    currentStep++;
+                }
+            }
+            if(simState.getPos() == ps.getN()){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static int sampleMoveDistance(ProblemSpec ps, State state) {
+
+        double[] moveProbs = Util.getMoveProb(ps, state);
+
+        double p = Math.random();
+        double pSum = 0;
+        int move = 0;
+        for (int k = 0; k < ProblemSpec.CAR_MOVE_RANGE; k++) {
+            pSum += moveProbs[k];
+            if (p <= pSum) {
+                move = ps.convertIndexIntoMove(k);
+                break;
+            }
+        }
+        return move;
+    }
+
     public static void main(String[] args) {
+
+        String input = "examples/level_3/input_lvl3_better_cars.txt";
+        String output = "outputs/test.txt";
+
+        if (args.length != 2) {
+            System.out.println("Usage: java ProgramName inputFileName outputFileName");
+            //System.exit(2);
+        } else {
+            input = args[0];
+            output = args[1];
+        }
 
         ProblemSpec ps;
         try {
-            ps = new ProblemSpec("examples/level_2/input_lvl2.txt");
-            run(ps, "outputs/test.txt");
+            ps = new ProblemSpec(input);
+            run(ps, output);
 //            System.out.println(ps.toString());
         } catch (IOException e) {
             System.out.println("IO Exception occurred");
